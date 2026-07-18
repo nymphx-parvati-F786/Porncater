86f885652ba17d1c8598a19ccf931905be5b0d23
nymphx-parvati-F786 <deviparvatilovemuslimcocks@gmail.com>
2026-07-18 18:56:36 +0530

    Email testing run 2

--- app/admin/inbox/page.tsx
+++ app/admin/inbox/page.tsx
@@ -1,10 +1,11 @@
 // import InboxClientLayout from "./InboxClientLayout";
 
-// export const metadata = {
-//   title: "Admin Inbox - Porncater",
-// };
+export const metadata = {
+  title: "Admin Inbox - Porncater",
+};
 
-// export default function InboxPage() {
-//   // You can do server-side auth checks right here before rendering the client layout
-//   return <InboxClientLayout />;
-// }
+export default function InboxPage() {
+  // You can do server-side auth checks right here before rendering the client layout
+  // return <InboxClientLayout />;
+  return <h1>Admin Inbox</h1>;
+}
